7afaadc627318291ede6e6a476b272e6e001d326
Tobi Akinmusuru <91935741+Takinmu1@users.noreply.github.com>
2021-12-30 21:05:09 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
 library(dplyr)
 
 # Checking if archive already exists
-fileused<- "Coursera_GCD_Project.zip"
+filename<- "Coursera_GCD_Project.zip"
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, fileused)
+download.file(fileUrl, filename)
 
 # Checking if folder exists
 if (!file.exists("UCI HAR Dataset")) { 
@@ -14,35 +14,37 @@ if (!file.exists("UCI HAR Dataset")) {
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 xtable <- rbind(x_train, x_test)
 ytable <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, ytable, xtable)
+Mergedfiles <- cbind(Subject, ytable, xtable)
 
-NewData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+combineddata <- Mergedfiles %>% select(subject, code, contains("mean"), contains("std"))
 
-NewData$code <- activities[NewData$code, 2]
+combineddata$code <- activities[combineddata$code, 2]
 
-names(NewData)[2] = "activity"
-names(NewData)<-gsub("Acc", "Accelerometer", names(NewData))
-names(NewData)<-gsub("Gyro", "Gyroscope", names(NewData))
-names(NewData)<-gsub("BodyBody", "Body", names(NewData))
-names(NewData)<-gsub("Mag", "Magnitude", names(NewData))
-names(NewData)<-gsub("^t", "Time", names(NewData))
-names(NewData)<-gsub("^f", "Frequency", names(NewData))
-names(NewData)<-gsub("tBody", "TimeBody", names(NewData))
-names(NewData)<-gsub("-mean()", "Mean", names(NewData), ignore.case = TRUE)
-names(NewData)<-gsub("-std()", "STD", names(NewData), ignore.case = TRUE)
-names(NewData)<-gsub("-freq()", "Frequency", names(NewData), ignore.case = TRUE)
-names(NewData)<-gsub("angle", "Angle", names(NewData))
-names(NewData)<-gsub("gravity", "Gravity", names(NewData))
+names(combineddata)[2] = "activity"
+names(combineddata)<-gsub("Acc", "Accelerometer", names(combineddata))
+names(combineddata)<-gsub("angle", "Angle", names(combineddata))
+names(combineddata)<-gsub("BodyBody", "Body", names(combineddata))
+names(combineddata)<-gsub("^f", "Frequency", names(combineddata))
+names(combineddata)<-gsub("-freq()", "Frequency", names(combineddata), ignore.case = TRUE)
+names(combineddata)<-gsub("gravity", "Gravity", names(combineddata))
+names(combineddata)<-gsub("Gyro", "Gyroscope", names(combineddata))
+names(combineddata)<-gsub("Mag", "Magnitude", names(combineddata))
+names(combineddata)<-gsub("-mean()", "Mean", names(combineddata), ignore.case = TRUE)
+names(combineddata)<-gsub("-std()", "STD", names(combineddata), ignore.case = TRUE)
+names(combineddata)<-gsub("^t", "Time", names(combineddata))
+names(combineddata)<-gsub("tBody", "TimeBody", names(combineddata))
 
-FinalData <- NewData %>%
+
+
+FinalData <- combineddata %>%
         group_by(subject, activity) %>%
         summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
@@ -50,4 +52,3 @@ write.table(FinalData, "FinalData.txt", row.name=FALSE)
 str(FinalData)
 
 FinalData
-
